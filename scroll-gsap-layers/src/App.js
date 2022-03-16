c8a279de5e2002d584f90425dfb3fa17551ac846
mramgolam <mahima.ramgolam@prodigious.com>
2022-03-16 20:59:06 +0400

    added animations to layers

--- scroll-gsap-layers/src/App.js
+++ scroll-gsap-layers/src/App.js
@@ -1,143 +1,244 @@
-import "./index.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+import Header from "./Header";
 import Layer from "./Layer";
 import Sections from "./Sections";
-import { useEffect, useRef } from "react";
-import Header from "./Header";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
+import "./index.css";
 
 function App() {
-  gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
+  gsap.registerPlugin(ScrollTrigger);
   gsap.defaults({ ease: "none" });
 
   useEffect(() => {
-    let path = document.querySelector("path");
-    console.log(path);
-    let pathLength = path.getTotalLength();
-    console.log(pathLength);
+    let circle1 = document.querySelector(".circle1");
+    let circle2 = document.querySelector(".circle2");
+    let circle3 = document.querySelector(".circle3");
+    let circle4 = document.querySelector(".circle4");
+    let circle5 = document.querySelector(".circle5");
 
+    let layerTwo = document.querySelector('.layerTwo');
+
+    let path = document.querySelector("path");
+    let pathLength = path.getTotalLength();
     path.style.strokeDasharray = pathLength + " " + pathLength;
     path.style.strokeDashoffset = pathLength;
+
+    let countCircle5 = 0;
+    let countCircle4 = 0;
+    let countCircle3 = 0;
+    let countCircle2 = 0;
+    let countCircle1 = 0;
+
+    circle1.style.fill = "none";
+    circle2.style.fill = "none";
+    circle3.style.fill = "none";
+    circle4.style.fill = "none";
+    circle5.style.fill = "none";
+
 
     window.addEventListener("scroll", () => {
       // what % down is it ?
       let scrollPercentage =
-        (document.documentElement.scrollTop + document.body.scrollTop) /
-        (document.documentElement.scrollHeight -
-          document.documentElement.clientHeight);
-      console.log("scrollPercentage", scrollPercentage);
+        (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight -  document.documentElement.clientHeight);
+      let roundOff = Math.round(scrollPercentage * 100)
+      console.log(roundOff)
       //Length to offset the dashes
       let drawLength = pathLength * scrollPercentage;
-      //Draw in rverse
+      //Draw in reverse
       path.style.strokeDashoffset = pathLength - drawLength;
-
-      let circle1 = document.querySelector(".circle1");
-      let circle2 = document.querySelector(".circle2");
-      let circle3 = document.querySelector(".circle3");
-      let circle4 = document.querySelector(".circle4");
-      let circle5 = document.querySelector(".circle5");
-      if (scrollPercentage >= "0.11") {
+     
+      // layer 1 animations
+      if (roundOff > 3) {
         circle1.style.fill = "#D36226";
-      }
-      if (scrollPercentage >= "0.27") {
+        if(countCircle1 === 0 ){
+          countCircle1 = countCircle1 + 1;         
+          gsap.to(circle1, {
+            scrollTrigger: {
+              trigger: circle1,
+              start: "top 100px",
+              end: "top 300px",
+              scrub: true,
+              onEnter: () => {
+              },
+              onLeaveBack: () => {
+                circle1.style.fill = "none";
+              },
+            }
+        })}
+      } 
+      // layer 2 animations
+      if (roundOff > 23) {
         circle2.style.fill = "#2F8355";
-      }
-      if (scrollPercentage >= "0.45") {
-        circle3.style.fill = "#4D2682";
-      }
-      if (scrollPercentage >= "0.65") {
-        circle4.style.fill = "#982246";
-      }
-      if (scrollPercentage >= "0.8") {
-        circle5.style.fill = "#BA1719";
-      }
+        if(countCircle2 === 0 ){
+          countCircle2 = countCircle2 + 1;
+          gsap.to(circle2, {
+            scrollTrigger: {
+              trigger: circle2,
+              start: "top 300px",
+              end: "top 400px",
+              scrub: true,
+              onEnter: () => {
+                layerTwo.classList.add('bg-teal-500');
+                
+              },
+              
+              onLeaveBack: () => {
+                circle2.style.fill = "none";
+                layerTwo.classList.remove('bg-teal-500');
+              },
+            }
+          })
 
-      // switch (scrollPercentage) {
-      //   case 0.11846496106785318:
-      //     console.log("1111111111111111");
-      //     circle1.style.fill = "#D36226";
-      //     break;
-      //   case 0.27:
-      //     circle2.style.fill = "#2F8355";
-      //     break;
-      //   case 0.35:
-      //     circle3.style.fill = "#4D2682";
-      //     break;
-      //   case 0.62:
-      //     circle4.style.fill = "#982246";
-      //     break;
-      //   case 0.72:
-      //     circle5.style.fill = "#BA1719";
-      //     break;
-      //   default:
-      //     break;
-      // }
+          let section2 = document.querySelector('.section2');
+          section2.classList.remove('hidden')
+          gsap.fromTo(".layerTwo-right", {opacity: 0, x: -100}, {opacity: 1, x: 0, delay: 0.5, duration: 1.5})
+          gsap.fromTo(".layerTwo-left", {opacity: 0, x: 100}, {opacity: 1, x: 0, delay: 0.5, duration: 1.8})
+
+          gsap.fromTo(".layerTwo", {opacity: 0}, {opacity: 1, duration: 1.0, ease:'none'})
+      }
+      }
+      // layer 3 animations
+      if (roundOff > 43) {
+        circle3.style.fill = "#4D2682";
+        let layerThree = document.querySelector('.layerThree');
+        layerThree.classList.add('bg-purple-400');
+        if(countCircle3 === 0 ){
+          countCircle3 = countCircle3 + 1;
+          gsap.to(circle3, {
+            scrollTrigger: {
+              trigger: circle3,
+              start: "top 600px",
+              end: "top 690px",
+              scrub: true,
+              onEnter: () => {
+              },
+              onLeaveBack: () => {
+                circle3.style.fill = "none";
+                layerThree.classList.remove('bg-purple-400');
+              },
+            
+            }
+          })
+
+          let section3 = document.querySelector('.section3');
+          section3.classList.remove('hidden')
+          gsap.fromTo(".layerThree-right", {opacity: 0, x: -100}, {opacity: 1, x: 0, delay: 0.5, duration: 1.8})
+          gsap.fromTo(".layerThree-left", {opacity: 0, x: 100}, {opacity: 1, x: 0, delay: 0.5, duration: 1.5})
+
+          gsap.fromTo(".layerThree", {opacity: 0}, {opacity: 1, duration: 1.0, ease:'none'})
+        }
+      }
+      // layer 4 animations
+      if (roundOff > 63) {
+        circle4.style.fill = "#982246";
+        let layerFour = document.querySelector('.layerFour');
+        layerFour.classList.add('bg-pink-400');
+        if(countCircle4 === 0 ){
+          countCircle4 = countCircle4 + 1;
+          gsap.to(circle4, {
+            scrollTrigger: {
+              trigger: circle4,
+              start: "top 700px",
+              end: "top 400px",
+              scrub: true,
+              onEnter: () => {
+              },
+              onLeaveBack: () => {
+                circle4.style.fill = "none";
+                layerFour.classList.remove('bg-pink-400');
+              }
+            }
+          })
+
+          let section4 = document.querySelector('.section4');
+          section4.classList.remove('hidden')
+          gsap.fromTo(".layerFour-right", {opacity: 0, x: -100}, {opacity: 1, x: 0, delay: 0.5, duration: 1.5})
+          gsap.fromTo(".layerFour-left", {opacity: 0, x: 100}, {opacity: 1, x: 0, delay: 0.5, duration: 1.8})        
+
+          gsap.fromTo(".layerFour", {opacity: 0}, {opacity: 1, duration: 1.0, ease:'none'})
+        }
+      }
+      // layer 5 animations
+      if (roundOff > 88) {
+        circle5.style.fill = "#BA1719";
+        if(countCircle5 === 0 ){
+          countCircle5 = countCircle5 + 1;
+          let layerFive = document.querySelector('.layerFive');
+          console.log('layerFive', layerFive)
+          layerFive.classList.add('bg-red-500');
+          gsap.to(circle5, {
+            scrollTrigger: {
+              trigger: circle5,
+              start: "top 750px",
+              end: "top 690px",
+              scrub: true,
+              onEnter: () => {
+                console.log("enter")
+                layerFive.classList.add('bg-red-500');
+              },
+              onLeaveBack: () => {
+                console.log("leave back")
+                layerFive.classList.remove('bg-red-500');
+                circle5.style.fill = "none";
+              },
+            }
+          })
+
+          let section5 = document.querySelector('.section5');
+          section5.classList.remove('hidden')
+          gsap.fromTo(".layerFive-right", {opacity: 0, x: -100}, {opacity: 1, x: 0, delay: 0.5, duration: 1.5})
+          gsap.fromTo(".layerFive-left", {opacity: 0, x: 100}, {opacity: 1, x: 0, delay: 0.5, duration: 1.8})
+
+          gsap.fromTo(".layerFive", {opacity: 0}, {opacity: 1, duration: 1.0, ease:'none'})
+        }
+      }
     });
+
+    gsap.fromTo(".layerOne", {opacity: 0}, {opacity: 1, duration: 2})
+    gsap.fromTo(".sectionOne", {opacity: 0, x: -100}, {opacity: 1, duration: 1, x: 0})
   }, []);
 
   return (
     <div className="App">
       <Header></Header>
-      <div className="line-container">
-        <svg
-          width="147"
-          height="881"
-          viewBox="0 0 147 881"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M130 0V117V127.5L20 257.5L130 396L20 558L130 686V880.5"
-            stroke="black"
-            strokeWidth="4"
-          />
-          <ellipse
-            className="circle1"
-            cx="128.5"
-            cy="126.5"
-            rx="14.5"
-            ry="15.5"
-            fill="white"
-          />
-          <circle
-            className="circle2"
-            cx="16.5"
-            cy="255.5"
-            r="16.5"
-            fill="white"
-          />
-          <circle
-            className="circle3"
-            cx="126.5"
-            cy="396.5"
-            r="16.5"
-            fill="white"
-          />
-          <circle
-            className="circle4"
-            cx="20.5"
-            cy="558.5"
-            r="16.5"
-            fill="white"
-          />
-          <circle
-            className="circle5"
-            cx="130.5"
-            cy="682.5"
-            r="16.5"
-            fill="white"
-          />
+      <div className="line-container z-10">
+        <svg width="200" height="702" viewBox="0 0 155 702" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M105.5 0.5C-195 186 364.5 468.5 61 701.5" stroke="black"/>
+          <circle className="circle1" cx="79" cy="14" r="14" fill="none"/>
+          <circle className="circle2" cx="14" cy="160" r="14" fill="none"/>
+          <circle className="circle3" cx="70" cy="297" r="14" fill="none"/>
+          <circle className="circle4" cx="140" cy="449" r="14" fill="none"/>
+          <circle className="circle5" cx="126" cy="627" r="14" fill="none"/>
         </svg>
       </div>
-      <Layer name="one" color="bg-orange-400">
-        <Sections styling="flex justify-center items-center">
-          Layer one
+      <Layer name="layerOne" color="bg-orange-400">
+        <Sections styling="sectionOne flex justify-center items-center">
+          Digital!
         </Sections>
       </Layer>
-      <Layer name="two">
-        <Sections styling="flex justify-center items-center">
-          Layer two
+      <Layer name="layerTwo">
+        <Sections styling="section2 hidden flex justify-center items-center gap-8">
+          <img className="layerTwo-right" src="https://picsum.photos/500/300?grayscale" alt="example"></img>
+          <div className="layerTwo-left text-7xl">QA</div>
+        </Sections>
+      </Layer>
+      <Layer name="layerThree">
+        <Sections styling="section3 hidden flex justify-center items-center gap-8">
+          <div className="layerThree-right text-7xl">VIDEO</div>
+          <img className="layerThree-left" src="https://picsum.photos/500/300?grayscale" alt="example"></img>
+        </Sections>
+      </Layer>
+      <Layer name="layerFour">
+        <Sections styling="section4 hidden flex justify-center items-center gap-8">
+          <img className="layerFour-right" src="https://picsum.photos/500/300?grayscale" alt="example"></img>
+          <div className="layerFour-left text-7xl">Print</div>
+        </Sections>
+      </Layer>
+      <Layer name="layerFive">
+      <Sections styling="section5 hidden flex justify-center items-center gap-8">
+          <img className="layerFive-right" src="https://picsum.photos/500/300?grayscale" alt="example"></img>
+          <div className="layerFive-left text-7xl">CGI</div>
         </Sections>
       </Layer>
     </div>
@@ -145,3 +246,6 @@ function App() {
 }
 
 export default App;
+
+
+
--- scroll-gsap-layers/src/Layer.js
+++ scroll-gsap-layers/src/Layer.js
@@ -2,12 +2,6 @@ import React from "react";
 
 export default function Layer({children , name,  color, id}) {
   return (
-    <div
-      className={
-        "layer " + color + " " + id + " " + name
-      }
-    >
-      {children}
-    </div>
+    <div className={"layer " + color + " " + id + " " + name}>{children}</div>
   );
 }
